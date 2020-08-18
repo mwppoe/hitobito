82f85e762e828191685e7334fe03cc54e679a828
Mathis Hofer <hofer@puzzle.ch>
2020-08-18 17:36:04 +0200

    WIP: Add dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,40 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+/**
+ * Dependencies
+ */
+import jQuery from 'jquery';
+import 'js-cookie';
+import 'jquery-ujs';
+// import 'jquery-ui/widgets/datepicker';
+// import 'jquery-ui-datepicker-i18n';
+// import 'jquery-ui/effects/effect-highlight';
+import 'bootstrap-sass/js/bootstrap-transition';
+import 'bootstrap-sass/js/bootstrap-alert';
+import 'bootstrap-sass/js/bootstrap-button';
+import 'bootstrap-sass/js/bootstrap-collapse';
+import 'bootstrap-sass/js/bootstrap-dropdown';
+import 'bootstrap-sass/js/bootstrap-tooltip';
+import 'bootstrap-sass/js/bootstrap-popover';
+import 'bootstrap-sass/js/bootstrap-typeahead';
+import 'bootstrap-sass/js/bootstrap-tab';
+import 'bootstrap-sass/js/bootstrap-modal';
+// import 'bootstrap3-wysihtml5-bower';
+// TODO: jquery_nested_form
+import 'chosen-js';
+// TODO: jquery.remotipart
+// TODO: modernizr
+import 'moment'; // TODO: make global?
+// TODO: custom scripts
+// TODO: wysiwyg script
+// TODO: wagon script
+// import 'jquery.turbolinks';
+
+// Provide the jQuery object globally
+window.$ = jQuery
+window.jQuery = jQuery
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
